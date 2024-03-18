85ca50ee6d4c468cf36222fa3c7a53f1a8e4db9d
Dnyaneshwar Komatwar <dnyaneshwar.komatwar@bajajfinserv.in>
2024-03-18 11:59:06 +0530

    1st commit

---
+++ fsd-loan-business/src/main/java/com/fsd/loan/FsdLoanBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.fsd.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsdLoanBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsdLoanBusinessApplication.class, args);
+	}
+
+}
